97b51203044c52553388a085534767f6e4b66541
Musa Lawrence Ilemona <musa.lawrence93@gmail.com>
2023-03-03 13:53:55 +0100

    Added a single IBAction for the three egg type buttons and a Timer function for the egg timer

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let eggTimes: [String : Int] = ["Soft": 5, "Medium": 8, "Hard": 12]
+    
+    var eggCookingTime: Int?
+    
+    var timer = Timer()
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
 
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        titleLabel.text = "Cooking egg!"
+        let hardness = sender.currentTitle!
+        timer.invalidate()
+        
+        switch(hardness){
+        case "Soft":
+            eggCookingTime = eggTimes["Soft"]!
+            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        case "Medium":
+            eggCookingTime = eggTimes["Medium"]!
+            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        case "Hard":
+            eggCookingTime = eggTimes["Hard"]!
+            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        default:
+            print("undefined hardness")
+        }
+    }
 
+    @objc func updateTimer() {
+        if(eggCookingTime! > 0) {
+            eggCookingTime! -= 1
+            print(eggCookingTime!)
+        } else if (eggCookingTime == 0){
+            titleLabel.text = "Done!"
+        }
+    }
 }
