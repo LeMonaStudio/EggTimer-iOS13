7f3cb691418e1fd79a720268f4b0d6cb2b0a84a8
Musa Lawrence Ilemona <musa.lawrence93@gmail.com>
2023-03-04 11:34:53 +0100

    Added a progressView to show the User the progress of the cooking egg timer. Also added alarm sound that would be played when the egg completes cooking.

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,48 +7,71 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
-    let eggTimes: [String : Int] = ["Soft": 5, "Medium": 8, "Hard": 12]
-    
+    let eggTimes: [String : Int] = ["Soft": 300, "Medium": 420, "Hard": 720]
     var eggCookingTime: Int?
-    
     var timer = Timer()
+    var progressIncrement: Float = 0.0
+    var player: AVAudioPlayer?
     
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var progressView: UIProgressView!
+    
     
     
     override func viewDidLoad() {
+        progressView.progress = 0.0
         super.viewDidLoad()
     }
 
+    
     @IBAction func hardnessSelected(_ sender: UIButton) {
-        titleLabel.text = "Cooking egg!"
         let hardness = sender.currentTitle!
+        
+        progressView.progress = 0.0
+        titleLabel.text = "Cooking \(hardness) egg!"
+        
         timer.invalidate()
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
         
         switch(hardness){
         case "Soft":
             eggCookingTime = eggTimes["Soft"]!
-            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
         case "Medium":
             eggCookingTime = eggTimes["Medium"]!
-            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
         case "Hard":
             eggCookingTime = eggTimes["Hard"]!
-            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
         default:
             print("undefined hardness")
         }
+        
+        progressIncrement = 1.0 / Float(eggCookingTime!)
     }
 
     @objc func updateTimer() {
         if(eggCookingTime! > 0) {
             eggCookingTime! -= 1
-            print(eggCookingTime!)
+            progressView.progress += progressIncrement
         } else if (eggCookingTime == 0){
+            timer.invalidate()
+            progressView.progress = 1.0
+            playSound(resourceName: "alarm_sound")
             titleLabel.text = "Done!"
         }
+    }
+    
+    func playSound(resourceName: String) {
+        guard let url = Bundle.main.url(forResource: resourceName, withExtension: "mp3") else {
+            return
+        }
+        
+        do {
+            player = try! AVAudioPlayer(contentsOf: url)
+            player?.play()
+        }
+        
     }
 }
